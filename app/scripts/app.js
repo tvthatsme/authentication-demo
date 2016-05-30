3e36bcd248298a83453e4913ba0913acc6ca7be2
Timothy Vernon <timothy.n.vernon@gmail.com>
2016-05-30 16:46:57 -0400

    .gitignore created by brackets-git extension

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name authenticationDemoApp
+ * @description
+ * # authenticationDemoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('authenticationDemoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name authenticationDemoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the authenticationDemoApp
+ */
+angular.module('authenticationDemoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
