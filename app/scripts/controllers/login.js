ec3ef914e3a9368edceb00e86902038268e6753a
Timothy Vernon <timothy.n.vernon@gmail.com>
2016-06-01 20:28:48 -0400

    Add form validation for login.

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,10 +8,20 @@
  * Controller of the authenticationDemoApp
  */
 angular.module('authenticationDemoApp')
-  .controller('LoginCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('LoginCtrl', ['$scope', function ($scope) {
+    
+    // Define the maximum allowable number of characters for the username
+    $scope.usernameInputMax = 100;
+    
+    // Define the minimum allowable number of characters for the password
+    $scope.passwordInputMin = 7;
+    
+    // Define the maximum allowable number of characters for the password
+    $scope.passwordInputMax = 25;
+    
+    // Define an empty username for the login form
+    $scope.usersname = '';
+    
+    // Define an empty password for the login form
+    $scope.password = '';
+  }]);
